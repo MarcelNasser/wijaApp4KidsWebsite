c097ed8e3749fae4d454e1d77b16573ab17f9729
marcel ndeffo <marcel.ndeffo@gmail.com>
2019-02-27 08:57:12 +0100

    fix table animation beug

--- js/fixStyle.js
+++ js/fixStyle.js
@@ -476,7 +476,7 @@
 			}
 			if (mailBoxItems!=null){
 				for (var j = 0; j < mailBoxItems.length; j++){
-					if (j%4<2){mailBoxItems[j].classList.remove("blueBeat");}
+					if (j%4==0|j%4==3){mailBoxItems[j].classList.remove("blueBeat");}
 					else{mailBoxItems[j].classList.remove("redBeat");}
 				}
 			} 
